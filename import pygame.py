2e14ae4b38d3c60159926bf4eb36bd68faba4c8c
Jon <BAST03@stpiusx.nsw.edu.au>
2026-05-28 15:05:18 +1000

    end of lesson

--- import pygame.py
+++ import pygame.py
@@ -1,5 +1,15 @@
 import pygame
 import random
-import math
 
+pygame.font.init()
+
+# global variables
+s_with = 800
+s_height = 700
+play_width = 300
+play_height = 600
+block_size = 30
+
+top_left_x = (s_with - play_width) // 2
+top_left_y = s_height - play_height
 
